796a100bdbc5799f29355a938146ffc1cc961cb7
mpratdev <mpratmaya@gmail.com>
2024-04-27 23:08:48 +0200

    articles information json

---
+++ src/pages/news/news.js
@@ -0,0 +1,100 @@
+import {
+    
+} from "../../../../public/news/viper.json";
+
+const { blackwidow, deathstalker, huntsman } = razer;
+const { gpro, g513, g910 } = logitech;
+const { one2mini, originvintage, outlaw65 } = ducky;
+
+export const pages = [
+    {
+        keyboard: "blackwidow",
+        name: blackwidow.name,
+        description: blackwidow.description,
+        image: blackwidow.image,
+        originalPrice: blackwidow["original-price"],
+        offertPrice: blackwidow["offert-price"],
+        brand: blackwidow.brand,
+        type: blackwidow.type,
+    },
+    {
+        keyboard: "deathstalker",
+        name: deathstalker.name,
+        description: deathstalker.description,
+        image: deathstalker.image,
+        originalPrice: deathstalker["original-price"],
+        offertPrice: deathstalker["offert-price"],
+        brand: deathstalker.brand,
+        type: deathstalker.type,
+    },
+    {
+        keyboard: "huntsman",
+        name: huntsman.name,
+        description: huntsman.description,
+        image: huntsman.image,
+        originalPrice: huntsman["original-price"],
+        offertPrice: huntsman["offert-price"],
+        brand: huntsman.brand,
+        type: huntsman.type,
+    },
+    {
+        keyboard: "gpro",
+        name: gpro.name,
+        description: gpro.description,
+        image: gpro.image,
+        originalPrice: gpro["original-price"],
+        offertPrice: gpro["offert-price"],
+        brand: gpro.brand,
+        type: gpro.type,
+    },
+    {
+        keyboard: "g513",
+        name: g513.name,
+        description: g513.description,
+        image: g513.image,
+        originalPrice: g513["original-price"],
+        offertPrice: g513["offert-price"],
+        brand: g513.brand,
+        type: g513.type,
+    },
+    {
+        keyboard: "g910",
+        name: g910.name,
+        description: g910.description,
+        image: g910.image,
+        originalPrice: g910["original-price"],
+        offertPrice: g910["offer-price"],
+        brand: g910.brand,
+        type: g910.type,
+    },
+    {
+        keyboard: "one2mini",
+        name: one2mini.name,
+        description: one2mini.description,
+        image: one2mini.image,
+        originalPrice: one2mini["original-price"],
+        offertPrice: one2mini["offert-price"],
+        brand: one2mini.brand,
+        type: one2mini.type,
+    },
+    {
+        keyboard: "originvintage",
+        name: originvintage.name,
+        description: originvintage.description,
+        image: originvintage.image,
+        originalPrice: originvintage["original-price"],
+        offertPrice: originvintage["offert-price"],
+        brand: originvintage.brand,
+        type: originvintage.type,
+    },
+    {
+        keyboard: "outlaw65",
+        name: outlaw65.name,
+        description: outlaw65.description,
+        image: outlaw65.image,
+        originalPrice: outlaw65["original-price"],
+        offertPrice: outlaw65["offer-price"],
+        brand: outlaw65.brand,
+        type: outlaw65.type,
+    }
+];
